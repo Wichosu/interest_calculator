3fd3d187c0f9902b49dde8bf8aad990f0ebb1491
Wicho <luiscalderonmiranda@gmail.com>
2022-10-20 21:41:53 -0500

    added title component

---
+++ src/components/pure/title.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div>
+      <h1 className='display-1'>Calculadora de Intereses</h1>
+    </div>
+  );
+}
+
+export default Title;
