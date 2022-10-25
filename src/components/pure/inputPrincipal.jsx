d6c4f7e18b07316b330f341651a06fdcac59b7bf
Wicho <luiscalderonmiranda@gmail.com>
2022-10-24 21:19:55 -0500

    changes style on div for inputPrincipal.jsx

--- src/components/pure/inputPrincipal.jsx
+++ src/components/pure/inputPrincipal.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const inputPrincipal = () => {
   return (
-    <div className='row'>
+    <div className='mb-3'>
       <label className='my-3 text-start form-label' style={{fontSize: '1.5rem'}} htmlFor='input'>Principal</label>
       <input className='form-control' id='input'/>
     </div>
