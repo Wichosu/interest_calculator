7335014de555719d8ce6b3f4f14071b36858a0b2
Wicho <54341712+Wichosu@users.noreply.github.com>
2022-11-28 18:47:18 -0600

    styles for md screens

--- components/pure/Navbar.jsx
+++ components/pure/Navbar.jsx
@@ -13,8 +13,14 @@ function Navbar() {
   useEffect(() => {
     window
       .matchMedia("(max-width: 768px)")
-      .addEventListener('change', () => setMenu(false))
+      .addEventListener('change', () => setMenu(false));
   }, []);
+
+  function switchMenu() {
+    if(window.matchMedia("(max-width: 768px").matches){
+      setMenu(!menu);
+    } 
+  }
 
   const lngs = {
     en: { nativeName: 'English'},
@@ -32,82 +38,86 @@ function Navbar() {
       <div className='md:hidden sticky top-0'>
         <div className='flex items-center bg-slate-50 py-2'>
           <CgMenu 
-            onClick={() => setMenu(!menu)}
+            onClick={switchMenu}
             className='ml-6 text-2xl text-slate-900'
           />
           <div className='mx-auto text-xl text-slate-900'>Finance App</div>
         </div>
       </div>
-      <nav 
-        className={ menu ?
-          'fixed w-3/4 h-screen bg-slate-50 pt-4 px-6'
-          :
-          'md:block hidden'}
-      >
-        <ul>
-          <li className='flex place-content-evenly text-slate-600'>
-            {
-              Object.keys(lngs).map((lng) => {
-                const { pathname, query, asPath, locale } = router;
-                return (
-                  <Link
-                    key={lng}
-                    href={{pathname, query}}
-                    as={asPath}
-                    locale={lng}
-                    legacyBehavior
-                  >
-                    <button 
-                      className={`border rounded py-0 px-2
-                        ${locale === lng ? 
-                          `text-slate-300 border-slate-200 cursor-default` 
-                          : 
-                          `border-slate-400 cursor-pointer`
-                        }
-                      `}
-                      disabled={locale === lng}
+      <div className='md:border-r-2 md:w-1/5 md:px-4'>
+        <div className='md:sticky md:top-0'>
+          <nav 
+            className={ menu ?
+              'fixed w-3/4 h-screen bg-slate-50 pt-4 px-6'
+              :
+              'md:block hidden'}
+          >
+            <ul>
+              <li className='flex place-content-evenly text-slate-600 md:pt-8'>
+                {
+                  Object.keys(lngs).map((lng) => {
+                    const { pathname, query, asPath, locale } = router;
+                    return (
+                      <Link
+                        key={lng}
+                        href={{pathname, query}}
+                        as={asPath}
+                        locale={lng}
+                        legacyBehavior
+                      >
+                        <button 
+                          className={`border rounded py-0 px-2
+                            ${locale === lng ? 
+                              `text-slate-300 border-slate-200 cursor-default` 
+                              : 
+                              `border-slate-400 cursor-pointer`
+                            }
+                          `}
+                          disabled={locale === lng}
+                        >
+                          {lngs[lng].nativeName}
+                        </button>
+                      </Link>
+                    )
+                  })
+                }
+              </li>
+              <li className='text-slate-700 text-2xl my-4'>
+                { t('nav') }
+                <hr />
+              </li>
+              {
+                links.map((link, index) => {
+                  const { pathname } = router;
+                  return (
+                    <li 
+                      key={index}
+                      className='text-blue-500 mb-4'
                     >
-                      {lngs[lng].nativeName}
-                    </button>
-                  </Link>
-                )
-              })
-            }
-          </li>
-          <li className='text-slate-700 text-2xl my-4'>
-            Options
-            <hr />
-          </li>
-          {
-            links.map((link, index) => {
-              const { pathname } = router;
-              return (
-                <li 
-                  key={index}
-                  className='text-blue-500 mb-4'
-                >
-                  <Link 
-                    href={link.href}
-                    legacyBehavior
-                  >
-                    <button
-                      className={link.href === pathname ?
-                        'text-slate-300'
-                        :
-                        'hover:underline' 
-                      }
-                      onClick={() => setMenu(!menu)}
-                      disabled={link.href === pathname}
-                    >
-                      {link.name}
-                    </button>
-                  </Link>
-                </li>
-              )
-            })
-          }
-        </ul>
-      </nav>
+                      <Link 
+                        href={link.href}
+                        legacyBehavior
+                      >
+                        <button
+                          className={link.href === pathname ?
+                            'text-slate-300 text-start'
+                            :
+                            'hover:underline text-start' 
+                          }
+                          onClick={switchMenu}
+                          disabled={link.href === pathname}
+                        >
+                          {link.name}
+                        </button>
+                      </Link>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </nav>
+        </div>
+      </div>
     </>
   )
 }
